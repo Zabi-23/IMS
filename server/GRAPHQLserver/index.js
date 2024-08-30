6d66e1bbc9a956ddec231c004fd618cfc9c897e9
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-08-30 21:01:09 +0200

    GRAPHQLserver

---
+++ server/GRAPHQLserver/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+// MongoDB-connection
+const DB = process.env.DATABASE.replace(
+  "<db_PASSWORD>",
+  process.env.DATABASE_PASSWORD
+).replace("<db_NAME>", process.env.DB_NAME);
+
+mongoose
+  .connect(DB)
+  .then(() =>
+    console.log(`MongoDB connected successfully to ${process.env.DB_NAME}`)
+  )
+  .catch((err) => console.log("Error connecting to MongoDB:", err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
